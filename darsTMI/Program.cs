4750c9a81470f606c2f302061f6f98d4153a53f1
Munavvara <ummusakiina@gmail.com>
2024-10-31 11:15:56 +0500

    1-versiya yangi dastur yaratildi

---
+++ darsTMI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace darsTMI
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 24, b = 58;
+			
+
+
+		}
+	}
+}
